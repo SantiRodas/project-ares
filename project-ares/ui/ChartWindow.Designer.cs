e84a14ea4b7f57b198d8bdc26b103073c1324b06
Santiago <santiago.rodas1@correo.icesi.edu.co>
2021-02-21 13:37:12 -0500

    All charts ready

--- project-ares/ui/ChartWindow.Designer.cs
+++ project-ares/ui/ChartWindow.Designer.cs
@@ -35,6 +35,10 @@ namespace project_ares.ui
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChartWindow));
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -46,8 +50,18 @@ namespace project_ares.ui
             this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
@@ -76,7 +90,7 @@ namespace project_ares.ui
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(455, 124);
+            this.label2.Location = new System.Drawing.Point(455, 128);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(153, 13);
             this.label2.TabIndex = 2;
@@ -85,14 +99,14 @@ namespace project_ares.ui
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(472, 140);
+            this.comboBox1.Location = new System.Drawing.Point(472, 144);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(497, 167);
+            this.button1.Location = new System.Drawing.Point(497, 171);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
@@ -103,7 +117,7 @@ namespace project_ares.ui
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(480, 446);
+            this.comboBox2.Location = new System.Drawing.Point(480, 447);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 21);
             this.comboBox2.TabIndex = 6;
@@ -111,7 +125,7 @@ namespace project_ares.ui
             // comboBox3
             // 
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(480, 473);
+            this.comboBox3.Location = new System.Drawing.Point(480, 474);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(121, 21);
             this.comboBox3.TabIndex = 7;
@@ -119,7 +133,7 @@ namespace project_ares.ui
             // comboBox4
             // 
             this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(480, 500);
+            this.comboBox4.Location = new System.Drawing.Point(480, 501);
             this.comboBox4.Name = "comboBox4";
             this.comboBox4.Size = new System.Drawing.Size(121, 21);
             this.comboBox4.TabIndex = 8;
@@ -127,7 +141,7 @@ namespace project_ares.ui
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(455, 430);
+            this.label3.Location = new System.Drawing.Point(455, 431);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(172, 13);
             this.label3.TabIndex = 9;
@@ -135,7 +149,7 @@ namespace project_ares.ui
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(503, 527);
+            this.button2.Location = new System.Drawing.Point(505, 528);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 10;
@@ -154,17 +168,115 @@ namespace project_ares.ui
             series2.ChartArea = "ChartArea1";
             series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
             series2.Legend = "Legend1";
-            series2.Name = "s1";
+            series2.Name = "Series2";
             this.chart2.Series.Add(series2);
             this.chart2.Size = new System.Drawing.Size(437, 301);
             this.chart2.TabIndex = 11;
             this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(763, 12);
+            this.chart3.Name = "chart3";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series3";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(437, 301);
+            this.chart3.TabIndex = 12;
+            this.chart3.Text = "chart3";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(856, 353);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 17);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "0 - 559";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(856, 376);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(76, 17);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "600 - 1159";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(856, 399);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(82, 17);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "1200 - 1759";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(856, 422);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(82, 17);
+            this.radioButton4.TabIndex = 16;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "1800 - 2359";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1025, 376);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Generate";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(856, 447);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(244, 205);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(853, 336);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Select a item to filter the time";
             // 
             // ChartWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1212, 649);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.chart3);
             this.Controls.Add(this.chart2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
@@ -184,6 +296,8 @@ namespace project_ares.ui
             this.Text = "Chart\'s";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -202,5 +316,13 @@ namespace project_ares.ui
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
     }
 }
--- project-ares/ui/ChartWindow.cs
+++ project-ares/ui/ChartWindow.cs
@@ -49,47 +49,57 @@ namespace project_ares.ui
         private void button1_Click(object sender, EventArgs e)
         {
 
-            chart1.Series.Clear();
-
-            chart1.Titles.Clear();
-
-            char l = (char)comboBox1.SelectedItem;
-
-            int size = 0;
-
-            DataRowCollection rows = dataTable.Rows;
-
-            string word = "";
-
-            foreach (DataRow dr in rows)
+            if (comboBox1.SelectedItem != null)
             {
 
-                object[] data = dr.ItemArray;
+                chart1.Series.Clear();
 
-                word = (string)data[0];
+                chart1.Titles.Clear();
 
-                if (word[0].Equals(l))
+                char l = (char)comboBox1.SelectedItem;
+
+                int size = 0;
+
+                DataRowCollection rows = dataTable.Rows;
+
+                string word = "";
+
+                foreach (DataRow dr in rows)
                 {
-                    size++;
+
+                    object[] data = dr.ItemArray;
+
+                    word = (string)data[0];
+
+                    if (word[0].Equals(l))
+                    {
+                        size++;
+
+                    }
 
                 }
 
-            }
 
+                string[] series = { l.ToString() };
+                int[] valores = { size };
 
-            string[] series = {l.ToString()};
-            int[] valores = {size};
+                chart1.Titles.Add("Lost people with the letter " + l);
 
-            chart1.Titles.Add("Lost people with the letter " + l);
+                for (int i = 0; i < series.Length; i++)
+                {
 
-            for (int i = 0; i < series.Length; i++)
+                    Series serie = chart1.Series.Add(series[i]);
+
+                    serie.Label = valores[i].ToString();
+
+                    serie.Points.Add(valores[i]);
+
+                }
+
+            } else
             {
 
-                Series serie = chart1.Series.Add(series[i]);
-
-                serie.Label = valores[i].ToString();
-
-                serie.Points.Add(valores[i]);
+                MessageBox.Show("Select one item", "Attention");
 
             }
 
@@ -100,51 +110,168 @@ namespace project_ares.ui
         private void button2_Click(object sender, EventArgs e)
         {
 
-            chart2.Series.Clear();
-
-            chart2.Titles.Clear();
-
-            char l1 = (char)comboBox2.SelectedItem;
-            char l2 = (char)comboBox3.SelectedItem;
-            char l3 = (char)comboBox4.SelectedItem;
-
-            int sl1 = 0;
-            int sl2 = 0;
-            int sl3 = 0;
-
-            DataRowCollection rows = dataTable.Rows;
-
-            string word = "";
-
-            foreach (DataRow dr in rows)
+            if (comboBox2.SelectedItem != null || comboBox3.SelectedItem != null || comboBox4.SelectedItem != null)
             {
 
-                object[] data = dr.ItemArray;
+                chart2.Series.Clear();
 
-                word = (string)data[0];
+                chart2.Titles.Clear();
 
-                if (word[0].Equals(l1))
+                char l1 = (char)comboBox2.SelectedItem;
+                char l2 = (char)comboBox3.SelectedItem;
+                char l3 = (char)comboBox4.SelectedItem;
+
+                int sl1 = 0;
+                int sl2 = 0;
+                int sl3 = 0;
+
+                DataRowCollection rows = dataTable.Rows;
+
+                string word = "";
+
+                int count = 0;
+
+                foreach (DataRow dr in rows)
                 {
-                    sl1++;
 
-                } else if (word[0].Equals(l2))
-                {
-                    sl2++;
+                    count++;
 
-                } else if (word[0].Equals(l3))
-                {
-                    sl3++;
+                    object[] data = dr.ItemArray;
+
+                    word = (string)data[1];
+
+                    if (word[0].Equals(l1))
+                    {
+                        sl1++;
+
+                    }
+                    else if (word[0].Equals(l2))
+                    {
+                        sl2++;
+
+                    }
+                    else if (word[0].Equals(l3))
+                    {
+                        sl3++;
+
+                    }
 
                 }
 
+                string[] series = {l1.ToString(), l2.ToString(), l3.ToString()};
+                int[] valores = {sl1, sl2, sl3};
+
+                chart2.Titles.Add("Lost people with the letter's " + l1 + ", " + l2 + ", " + l3 + ",  in the firts chart of the last name");
+
+                chart2.Series.Add("Series2");
+
+                chart2.Series["Series2"].ChartType = SeriesChartType.Pie;
+
+                for (int i = 0; i < series.Length; i++)
+                {
+
+                    chart2.Series["Series2"].Points.AddXY(series[i], valores[i]);
+
+                }
+
+                int t = count - (sl1 + sl2 + sl3);
+
+                chart2.Series["Series2"].Points.AddXY("Others", t);
+
+                chart2.Series["Series2"].Label = "#VALY";
+
+                chart2.Series["Series2"].LegendText = "#VALX";
+
+            } else
+            {
+
+                MessageBox.Show("Attention", "Select three item's");
+
             }
 
-            chart2.Titles.Add("Lost people with the letter's " + l1 + ", " + l2 + ", " + l3);
+        }
 
-            chart2.Series.Add("s1");
-            chart2.Series["s1"].Points.AddXY(sl1, 0.2);
-            chart2.Series["s1"].Points.AddXY(sl2, 0.5);
-            chart2.Series["s1"].Points.AddXY(sl3, 0.3);
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            chart3.Series.Clear();
+
+            chart3.Titles.Clear();
+
+            if (radioButton1.Checked || radioButton2.Checked || radioButton3.Checked || radioButton4.Checked)
+            {
+
+                DataRowCollection rows = dataTable.Rows;
+
+                int[] counts = new int[6];
+
+                int limitMin = 0;
+                int limitMax = 0;
+
+                int validation = 0;
+
+                if (radioButton1.Checked)
+                {
+                    limitMax = 559;
+
+                    validation = 1;
+
+                }
+                else if (radioButton2.Checked)
+                {
+                    limitMin = 600;
+                    limitMax = 1159;
+
+                    validation = 2;
+
+                }
+                else if (radioButton3.Checked)
+                {
+                    limitMin = 1200;
+                    limitMax = 1759;
+
+                    validation = 3;
+
+                }
+                else
+                {
+                    limitMin = 1800;
+                    limitMax = 2259;
+
+                    validation = 4;
+
+                }
+
+                foreach (DataRow dr in rows)
+                {
+                    int position = (int)(((int)dr["Time"] - limitMin) / 100);
+
+                    if (position >= 0 && position <= 5)
+                    {
+                        counts[position]++;
+                    }
+
+                }
+
+                chart3.Series.Add("Series3");
+                chart3.Series["Series3"].ChartType = SeriesChartType.Line;
+
+                for (int i = 0; i < counts.Length; i++)
+                {
+
+                    chart3.Series["Series3"].Points.AddXY(limitMin + i * 100, counts[i]);
+
+                }
+
+                chart3.Series["Series3"].Label = "#VALY";
+
+            }
+            else
+            {
+
+                MessageBox.Show("Select one item to filter", "Attention");
+
+            }
 
         }
 
